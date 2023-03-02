a43775ac88a99e948531c156b303271fb26b151d
Tyler Bifolchi <55516164+Tenor-Z@users.noreply.github.com>
2023-03-01 22:16:36 -0500

    Here it is!

--- poke_api.py
+++ poke_api.py
@@ -1,32 +1,32 @@
 import requests
 
-poke_api_URL = 'https://pokeapi.co/'
+poke_api_URL = 'https://pokeapi.co/'        #The URL for the PokeApi module (free to use)
 
 
 def main():  
-    pokemon = get_pokemon("cinderace")
+    pokemon = get_pokemon("vaporeon")          #Get the Pokemon named in the first argument (default is Vaporeon)
     print(pokemon) 
     
         
 def get_pokemon(search_term):
 
-    poke_Search_URL = f'https://pokeapi.co/api/v2/pokemon/{search_term}'
+    poke_Search_URL = f'https://pokeapi.co/api/v2/pokemon/{search_term}'             #This is the URL used to look for a Pokemon based on its name
 
 
-    header_params = {
+    header_params = {                                                   #Throw in these parameters when searching to limit the entity down to a Pokemon
         'Accept': 'application/json',
         'entity': 'pokemon',
     }
 
     print(f'Grabbing info about {search_term}...',  end=' ')
-    resp_msg = requests.get(poke_Search_URL, headers=header_params)
+    resp_msg = requests.get(poke_Search_URL, headers=header_params)     #Make our search combining the URL and the header parameters above
     
 
     if resp_msg.ok:
         print('Success!!')
-        body_dict = resp_msg.json()
-        poke_list = [ability['ability']['name']for ability in body_dict['abilities']]
-        return poke_list
+        body_dict = resp_msg.json()                                                     #Convert the data into a list
+        poke_list = [ability['ability']['name']for ability in body_dict['abilities']]      #For every ability in the data (everything with the name Abilities), using the name key for each, add it to a dictionary
+        return poke_list    
 
     else:
         print('Failure')
--- script.py
+++ script.py
@@ -10,9 +10,9 @@ import sys                              #We only need sys for cases where we run
 def main():
 
     search_term = get_search_term()                #We must get the argument given at the command line and return it so we can use it later
-    poke_list = get_pokemon(search_term)                #Use the returned Pokemon name to search for abilities
-    if poke_list:                                                     #If it is a valid Pokemon
-        title, body_text = get_paste_data(poke_list, search_term)      #Add the Pokemon and the abilities to the new paste as the title and body text
+    search_pokemon = get_pokemon(search_term)                #Use the returned Pokemon name to search for abilities
+    if search_pokemon:                                                     #If it is a valid Pokemon
+        title, body_text = get_paste_data(search_pokemon, search_term)      #Add the Pokemon and the abilities to the new paste as the title and body text
         paste_url = post_new_paste(title, body_text, '1M')           #And post it to Pastebin (By default it expires after 1 minute)
         print(f'URL of new paste: {paste_url}')                 #And display the newly made paste
     return
@@ -28,11 +28,11 @@ def get_search_term():
 
 
 
-def get_paste_data(poke_list, search_term):                 #This function nabs the paste data
+def get_paste_data(search_pokemon, search_term):                 #This function nabs the paste data
     search_term.title()                                         #First we must capitalize the pokemon's name
     title = (f'{search_term.title()}\'s Abilities')                 #This will be the title of the new paste
     divider =  '\n- '.join                                  #With the .join statement, we add a newline followed by a hyphen at the beginning
-    body_text = '- ' + divider(poke_list)                    #The body text will be each ability with the divider in effect
+    body_text = '- ' + divider(search_pokemon)                    #The body text will be each ability with the divider in effect
     return title, body_text                                 #And return this to main so it can be posted
 
 
